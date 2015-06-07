c7314435de441c0c7b54be2180a51d3a3cf8c340
Tung Dao <me@tungdao.com>
2015-06-07 11:28:18 +0700

    basic colorpicker working

--- components.js
+++ components.js
@@ -38,3 +38,119 @@ Ractive.components['collapsible'] = Ractive.extend({
     this.set('collapsed', !this.get('collapsed'));
   }
 });
+
+
+Ractive.components['colorpicker'] = Ractive.extend({
+  template: '#colorpicker',
+  isolated: true,
+  oninit: function() {
+    this.set({
+      picking: true,
+      value: this.get('value') || '#fff'
+    });
+
+    this.on('pick', this.pick.bind(this));
+    this.on('start', this.start.bind(this));
+    this.on('end', this.end.bind(this));
+    this.on('toggle', this.toggle.bind(this));
+
+    // document.addEventListener('click', this.end.bind(this), true);
+  },
+  onrender: function() {
+    this.draw(this.find('.colorpicker__pallete'));
+  },
+  toggle: function() {
+    this.fire(this.get('picking') ? 'end' : 'start');
+  },
+  start: function() {
+    this.set('picking', true);
+  },
+  end: function() {
+    this.set('picking', false);
+  },
+  pick: function(e) {
+    var x = e.original.layerX, y = e.original.layerY;
+
+    console.log(x, y, Rgb(x, y, 160, 160));
+
+    this.set({
+      picking: false,
+      value: Rgb(x, y, 160, 160)
+    });
+  },
+  draw: function(canvas) {
+    var ctx = canvas.getContext('2d'),
+        x, width = canvas.width,
+        y, height = canvas.height,
+        data = ctx.createImageData(width, height),
+        pixels = data.data,
+        i = 0, rgb;
+
+    for (y = 0; y < height; y += 1) {
+      for (x = 0; x < width; x += 1, i += 4) {
+        rgb = Rgb(x, y, width, height);
+
+        pixels[i] = rgb[0];
+        pixels[i + 1] = rgb[1];
+        pixels[i + 2] = rgb[2];
+        pixels[i + 3] = rgb[3] == 0 ? 0 : 255;
+      }
+    }
+
+    ctx.putImageData(data, 0, 0);
+  }
+});
+
+var Memoized = function(fn, self) {
+  var key, cache = {};
+
+  return function() {
+    key = [].slice.call(arguments).join('@');
+    if (!cache[key]) {
+      cache[key] = fn.apply(self, arguments);
+    }
+    return cache[key];
+  }
+};
+
+var Rgb = (function() {
+  var TAU = Math.PI * 2,
+      hue, sat, d, rx, ry, r;
+
+  function _Rgb(x, y, w, h) {
+    rx = x - w / 2 | 0;
+    ry = y - h / 2 | 0;
+    r = w / 2.1;
+    d = rx * rx + ry * ry;
+
+    if (d < r * r) {
+      hue = 6 * (Math.atan2(ry, rx) + Math.PI) / TAU;
+      sat = Math.sqrt(d) / r;
+
+      return HslToRgb(hue, sat, 255);
+    }
+
+    return [0, 0, 0, 0];
+  }
+
+  return Memoized(_Rgb);
+}());
+
+var HslToRgb = (function() {
+  var g, f, u, v, w, _r, _g, _b;
+
+  function _HslToRgb(hue, sat, l) {
+    g = Math.floor(hue);
+    f = hue - g;
+    u = l * (1 - sat);
+    v = l * (1 - sat * f);
+    w = l * (1 - sat * (1 - f));
+    _r = [l, v, u, u, w, l, l][g] | 0;
+    _g = [w, l, l, v, u, u, w][g] | 0;
+    _b = [u, u, w, l, l, v, u][g] | 0;
+
+    return [_r, _g, _b];
+  }
+
+  return Memoized(_HslToRgb);
+}());
